a1a297f4697c8645aa189f4f157919b83966bc86
Colin Loretz <colin.loretz@discordapp.com>
2026-02-10 14:13:53 -0800

    next docs migration

---
+++ scripts/docs.js
@@ -0,0 +1,3 @@
+if (window.location.hostname === 'discord.mintlify.app') { 
+    window.location.replace('https://docs.discord.com' + window.location.pathname + window.location.search + window.location.hash); 
+}
